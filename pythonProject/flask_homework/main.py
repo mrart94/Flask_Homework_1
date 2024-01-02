199ee4e24e1af011f2ed59a6b7ade1dd8df17a09
mrart94 <mr.art94@yandex.ru>
2024-01-02 23:57:20 +0300

    Второй коммит

--- pythonProject/flask_homework/main.py
+++ pythonProject/flask_homework/main.py
@@ -2,60 +2,58 @@ from flask import Flask
 import json
 
 
-# ЗАгружает данные из файла candidates.json:
+# Загружает данные из файла candidates.json:
 def load_candidates():
-    with open("candidates.json") as f:
-        temp = json.load(f)
-    return temp
+    with open("candidates.json", encoding='utf-8') as f:
+        json_to_dict = json.load(f)
+    return json_to_dict
 
-
-print(load_candidates())
-
-
+#Не пойму зачем она нужна?
 def get_all():
     print(load_candidates())
 
 
+#Получает кандидата в виде словаря
 def get_by_pk(pk):
-    for human in load_candidates():
-        if human['pk'] == pk:
-            return human
+    for candidate in load_candidates():
+        if candidate['pk'] == pk:
+            return candidate
 
 
+#Получает список кандидатов в виде списка, где каждый кандидат словарь, по названию скилла
 def get_by_skill(skill_name):
-    human_list = []
+    candidates_list_by_skill = []
     temp = load_candidates()
     for human in temp:
         if skill_name in (human['skills']).lower():
-            human_list.append(human)
-    return human_list
+            candidates_list_by_skill.append(human)
+    return candidates_list_by_skill
 
 
-print(get_by_skill('python'))
-
+#Начало программы
 app = Flask(__name__)
 
 
 @app.route("/")
 def list_human():
-    for human in load_candidates():
-        string_ = []
-        string_.append("Имя кандидата -" + human['name'] + "\n" + "Позиция кандидата" + human[
-            'position'] + "\n" + "Навыки через заяпятую" + human['skills'])
-    return string_
+    str_load_candidates = []
+    for candidate in load_candidates():
+        str_load_candidates.append(f'<pre>Имя кандидата - {candidate['name']}<br>Позиция кандидата {candidate['position']}<br>Навыки через заяпятую {candidate['skills']}</pre>')
+    return f'<html>{''.join(str_load_candidates)}</html>'
 
 
 @app.route("/candidates/<int:x>")
 def candida(x):
     temp = get_by_pk(x)
-    return f'<img src="{temp['picture']}">'
+    return f'<html><img src="({temp['picture']})"><pre>Имя кандидата - {temp['name']}<br>Позиция кандидата {temp['position']}<br>Навыки через заяпятую {temp['skills']}</pre></html>'
 
 
-@app.route("/skills/<int:x>")
+@app.route("/skills/<x>")
 def skills(x):
     temp = get_by_skill(x)
-    for i in temp:
-        pass
-
-
+    to_return = []
+    for candidate in temp:
+        to_return.append((f'<pre>Имя кандидата - {candidate['name']}<br>Позиция кандидата {candidate['position']}<br>Навыки через заяпятую {candidate['skills']}</pre>'))
+    return f'<html>{''.join(to_return)}</html>'
+#
 app.run()
