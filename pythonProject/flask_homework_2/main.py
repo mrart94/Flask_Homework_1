da1b0ec11ab089ca7d7a0f0f734de6fbf5d5d14a
mrart94 <32241914+mrart94@users.noreply.github.com>
2024-01-06 13:20:50 +0400

    Add files via upload

---
+++ pythonProject/flask_homework_2/main.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template
+
+from functions import *
+
+# Начало программы
+app = Flask(__name__)
+
+
+# Выводит мою карточку
+@app.route("/mi")
+def index():
+    return render_template("index.html", title="Dmitry")
+
+
+# Выводит всех кандидатов
+@app.route("/")
+def candidates():
+    temp = load_candidates_from_json()
+    return render_template("list.html", temp=temp)
+
+
+# Выводит карточку кандидата по id
+@app.route("/candidate/<int:x>")
+def candidate(x):
+    temp = get_candidate(x)
+    return render_template("card.html", temp=temp)
+
+
+# Выводит кандидатов с указанным именем
+@app.route("/search/<candidate_name>")
+def name_(candidate_name):
+    temp = get_candidates_by_name(candidate_name)
+    count = len(temp)
+    return render_template("search.html", count=count, temp=temp)
+
+
+# Выводит кандидатов с указанным скиллом
+@app.route("/skill/<skill_name>")
+def skill(skill_name):
+    temp = get_candidates_by_skill(skill_name)
+    count = len(temp)
+    return render_template("skill.html", count=count, temp=temp, skill_name=skill_name)
+
+
+app.run()
